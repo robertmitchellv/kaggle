a604a08cf076f19e4a311dae7fea7e16e662ecf4
robertmitchellv <robert.beaty.mitchell@gmail.com>
2014-12-22 15:40:40 -0800

    setting up the files for Titanic; need to do some more reading before continuing

--- Titanic/mySubmission.py
+++ Titanic/mySubmission.py
@@ -7,3 +7,24 @@
     Date: Dec 22, 2104
     Revised: Dec 22, 2014
 """
+
+import pandas as pd
+import numpy as np
+
+
+def main():
+    # comment
+    try:
+        train = pd.read_csv('Data/train.csv')
+    except IOError:
+        print("io ERROR-->Could not locate file.")
+    
+    # comment
+    try:
+        test = pd.read_csv('Data/test.csv')
+    except IOError:
+        print("io ERROR-->Could not locate file.")
+
+
+# call main function
+main()
